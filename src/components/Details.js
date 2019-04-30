2bc2c0ce82a03db3d41c2bc222e4c757c328b812
spiritxhx <spiritxhx@gmail.com>
2019-04-30 19:15:14 -0400

    create the Details component

---
+++ src/components/Details.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Details extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Hello from details</h3>
+      </div>
+    )
+  }
+}
