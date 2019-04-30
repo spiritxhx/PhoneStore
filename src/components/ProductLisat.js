ea353f2b9fe405e36911a0c9ef06080dea134960
spiritxhx <spiritxhx@gmail.com>
2019-04-30 18:07:39 -0400

    create the Product component

--- src/components/ProductLisat.js
+++ src/components/ProductLisat.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
+import Product from './Product';
 
 export default class ProductList extends Component {
     render() {
         return(
             <div>
-                <h3>Hello from Navbar</h3>
+                <Product />
             </div>
         );
     }
