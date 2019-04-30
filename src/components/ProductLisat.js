b4a1a28fc4827a25f70759ea7681c0fc6dde8e67
spiritxhx <spiritxhx@gmail.com>
2019-04-30 18:04:22 -0400

    create the ProductList component

--- src/components/ProductLisat.js
+++ src/components/ProductLisat.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 
-export default class Navbar extends Component {
+export default class ProductList extends Component {
     render() {
         return(
             <div>
