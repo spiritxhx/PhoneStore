e8a2865c055bc7b48451be19a217eb0dd785b316
spiritxhx <spiritxhx@gmail.com>
2019-05-01 23:18:02 -0400

    import the component in App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React from 'react';
 import logo, { ReactComponent } from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Cart from './components/Cart';
+import Default from './components/Default';
+
 
 class App extends React.Component{
   render()
   {
     return (
      <React.Fragment>
-       <h3>Hello from app</h3>
+       <Navbar />
      </React.Fragment> 
     );
   }
