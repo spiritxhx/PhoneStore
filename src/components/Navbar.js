887a9f9d7822aeac01cd1947c45efbe0dfaa2299
spiritxhx <spiritxhx@gmail.com>
2019-05-02 23:30:36 -0400

    add the home page link in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,13 +6,20 @@ import logo from '../logo.svg';
 export default class Navbar extends Component {
     render() {
         return(
-            <nav className='navbar navbar-expand-sm navbar-dark px-sm-5'>
-            {/* https://www.iconfinder.com/icons/1243689/call_phone_icon
+            <nav className='navbar navbar-expand-sm bg-primary navbar-dark px-sm-5'>
+                 {/* https://www.iconfinder.com/icons/1243689/call_phone_icon
 Creative Commons (Attribution 3.0 Unported);
 https://www.iconfinder.com/Makoto_msk */}
                 <Link to='/'>
                     <img src={logo} alt="store" className="navbar-brand" />
                 </Link>
+                <ul className="navbar-nav align-items-center">
+                    <li className="nav-item ml-5">
+                        <Link to="/" className="nav-link">
+                            products
+                        </Link>
+                    </li>
+                </ul>
             </nav>
         );
     }
