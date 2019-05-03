b89c160b53756fcd43fcd42af77ac24391317ef2
spiritxhx <spiritxhx@gmail.com>
2019-05-03 00:01:04 -0400

    make sure the button works the same

---
+++ src/components/Button.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    text-transform: capitalized;
+    font-size:1.4rem;
+    background: transparent;
+    border: 0.05rem solid var(--lightBlue);
+    color: var(--lightBlue);
+    border-redius: 0.5rem;
+    padding: 0.2rem 0.5rem; 
+    cursor: pointer;
+    margin: 0.2rem 0.5 rem 0.2rem 0;
+    transition: all 0.5s ease-in-out;
+    &:hover{
+        background: var(--lightBlue);
+        color: var(--mainBlue);
+    }
+    %:focus {
+        outline: none;
+    }
+`;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import logo from '../logo.svg';
-import styled from 'styled-components';
+import { ButtonContainer } from './Button';
 
 export default class Navbar extends Component {
     render() {
