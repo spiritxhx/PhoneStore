b69f5b0c971bb9e4601fbeb2ed6711e3e42da86b
spiritxhx <spiritxhx@gmail.com>
2019-05-02 23:25:01 -0400

    add the logo in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,19 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import logo from '../logo.svg';
+
 
 export default class Navbar extends Component {
     render() {
         return(
-            <div>
-                <h3>Hello from Navbar</h3>
-            </div>
+            <nav className='navbar navbar-expand-sm navbar-dark px-sm-5'>
+            {/* https://www.iconfinder.com/icons/1243689/call_phone_icon
+Creative Commons (Attribution 3.0 Unported);
+https://www.iconfinder.com/Makoto_msk */}
+                <Link to='/'>
+                    <img src={logo} alt="store" className="navbar-brand" />
+                </Link>
+            </nav>
         );
     }
 }
