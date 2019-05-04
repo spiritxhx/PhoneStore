408536695a07b7e8f2d4946e848f80c47f6c8901
spiritxhx <spiritxhx@gmail.com>
2019-05-03 22:31:11 -0400

    use styled-components to create css profile in navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,11 +2,12 @@ import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import logo from '../logo.svg';
 import { ButtonContainer } from './Button';
+import styled from 'styled-components';
 
 export default class Navbar extends Component {
     render() {
         return(
-            <nav className='navbar navbar-expand-sm bg-primary navbar-dark px-sm-5'>
+            <NavWrapper className='navbar navbar-expand-sm navbar-dark px-sm-5'>
                  {/* https://www.iconfinder.com/icons/1243689/call_phone_icon
 Creative Commons (Attribution 3.0 Unported);
 https://www.iconfinder.com/Makoto_msk */}
@@ -28,7 +29,11 @@ https://www.iconfinder.com/Makoto_msk */}
                         my cart
                     </ButtonContainer>
                 </Link>
-            </nav>
+            </NavWrapper>
         );
     }
 }
+
+const NavWrapper = styled.nav`
+    background:var(--mainBlue);
+`;
