e1252a6b38f31dc5f95c830f6aef28164c91d77f
spiritxhx <spiritxhx@gmail.com>
2019-05-04 23:15:28 -0400

    fix the capitalize typo

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const ButtonContainer = styled.button`
-    text-transform: capitalized;
+    text-transform: capitalize;
     font-size:1.4rem;
     background: transparent;
     border: 0.05rem solid var(--lightBlue);
