b27116d004572ea473dfaa8ac8456a0dd8042ad8
spiritxhx <spiritxhx@gmail.com>
2019-05-04 21:23:48 -0400

    add style to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -36,4 +36,9 @@ https://www.iconfinder.com/Makoto_msk */}
 
 const NavWrapper = styled.nav`
     background:var(--mainBlue);
+    .nav-link{
+        color:var(--mainWhite)!important;
+        font-size:1.3rem;
+        text-transform:capitalize!important;
+    }
 `;
