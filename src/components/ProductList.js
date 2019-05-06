44d2e02ed189e25c7c557a6788b4e5c8478a0105
spiritxhx <spiritxhx@gmail.com>
2019-05-06 19:12:29 -0400

    create a context.js as a data api

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import Product from './Product';
 import Title from './Title';
+import {storeProducts} from '../data';
 
 export default class ProductList extends Component {
     state={
-        products:[]
+        products:storeProducts
     };
     render() {
+        console.log(this.state.products);
         return(
             <React.Fragment>
                 <div className="py-5">
