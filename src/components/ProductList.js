cf15a9822a55b1275591c9c84a0bb888c9016020
spiritxhx <spiritxhx@gmail.com>
2019-05-06 23:54:28 -0400

    add functions andd contextProvider value

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -2,10 +2,11 @@ import React, {Component} from 'react';
 import Product from './Product';
 import Title from './Title';
 import {storeProducts} from '../data';
+import {ProductConsumer} from '../context';
 
 export default class ProductList extends Component {
     state={
-        products:storeProducts
+        products: storeProducts
     };
     render() {
         console.log(this.state.products);
@@ -15,7 +16,11 @@ export default class ProductList extends Component {
                     <div class="container">
                     <Title name="our" title="products" />
                         <div class="row">
-                            
+                            <ProductConsumer>
+                                {value => {
+                                    console.log(value);
+                                }}
+                            </ProductConsumer>
                         </div>
                     </div>
                 </div>
--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 import {BrowserRouter as Router} from 'react-router-dom';
 import {ProductProvider} from "./context";
 import * as serviceWorker from './serviceWorker';
-import { ProductProvider } from './context';
 
 ReactDOM.render(
     <ProductProvider>
