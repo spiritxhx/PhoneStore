0df587853478f3294f40a927d5d45b5526ce984c
spiritxhx <spiritxhx@gmail.com>
2019-05-07 21:40:58 -0400

    import the necessary components in Product.js

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+import {Link} from 'react-router-dom';
+import {ProductConsumer} from '../context';
 
 export default class Product extends Component {
   render() {
+    const {id, title, img, price, inCart} = this.props.product;
     return (
       <div>
         <h3>hello from product</h3>
