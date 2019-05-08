3a517c15eabcd6ceb3047800d490e5f726a066d8
spiritxhx <spiritxhx@gmail.com>
2019-05-07 23:55:31 -0400

    add the img to the product

--- src/components/Product.js
+++ src/components/Product.js
@@ -8,7 +8,13 @@ export default class Product extends Component {
     const {id, title, img, price, inCart} = this.props.product;
     return (
       <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
-        <h3>hello from product</h3>
+        <div className="card">
+          <div className="img-container p-5" onClick={console.log('You clicked on the image container')}>
+            <Link to="/details">
+            <img src={img} alt="product" className="card-img-top"></img>
+            </Link>
+          </div>
+        </div>
       </ProductWrapper>
     )
   }
