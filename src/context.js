4595dce9f5e0cb6f3078bfc218f1761e2a8cc879
spiritxhx <spiritxhx@gmail.com>
2019-05-08 23:51:40 -0400

    add the setProducts method to avoid refering data directly

--- src/context.js
+++ src/context.js
@@ -7,9 +7,22 @@ const ProductContext = React.createContext();
 
 class ProductProvider extends Component {
     state = {
-        products: storeProducts, 
+        products: [], 
         detailProduct: detailProduct
     };
+    componentDidMount() {
+        this.setProducts();
+    }
+    setProducts = () =>{
+        let products = [];
+        storeProducts.forEach(item =>{
+            const singleItem = {...item};
+            tempProducts = [...tempProducts,singleItem];
+        })
+        this.setState(()=>{
+            return {products:tempProducts}
+        })
+    }
     handleDetail =() => {
         console.log('hello from detail');
     };
