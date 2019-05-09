882d9c8588db83262477833162647bd8424ea76e
spiritxhx <spiritxhx@gmail.com>
2019-05-08 21:51:54 -0400

    biuld the add to cart button

--- src/components/Product.js
+++ src/components/Product.js
@@ -9,10 +9,13 @@ export default class Product extends Component {
     return (
       <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
         <div className="card">
-          <div className="img-container p-5" onClick={console.log('You clicked on the image container')}>
+          <div className="img-container p-5" onClick={() => console.log('You clicked on the image container')}>
             <Link to="/details">
             <img src={img} alt="product" className="card-img-top"></img>
             </Link>
+            <button className="cart-byn" disabled={inCart?true: false} onClick={()=>{console.log('added to the cart')} }>
+              {inCart?(<p className="text-capitalize mb-0" disabled>in Cart</p>):(<i className="fas fa-cart" />)}
+            </button>
           </div>
         </div>
       </ProductWrapper>
