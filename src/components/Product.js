7c765b593bca1ac4ae636cbf0603569178ed61f6
spiritxhx <spiritxhx@gmail.com>
2019-05-08 23:29:42 -0400

    add the hover effect to the cart btn

--- src/components/Product.js
+++ src/components/Product.js
@@ -81,9 +81,17 @@ const ProductWrapper = styled.div`
   padding: 0.2rem 0.4rem;
   background: var(--lightBlue);
   border: none;
-  color: var(-mainWhite);
+  color: var(--mainWhite);
   font-size:1.4rem;
   border-radius: 0.5rem 0 0 0;
   transform: translate(100%, 100%);
+  transition: all 1s linear;
+}
+.img-container:hover .cart-btn{
+  transform: translate(0, 0);
+}
+.cart-btn:hover {
+  color: var(--mainBlue);
+  cursor: pointer;
 }
 `;
