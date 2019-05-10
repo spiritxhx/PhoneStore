91f848fb8764506fc585812e1730c8e16946f14e
spiritxhx <spiritxhx@gmail.com>
2019-05-09 22:19:04 -0400

    add the detailProduct to Details.js

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
-import {ProtuctConsumer} from '../context';
+import {ProtuctConsumer, ProductConsumer} from '../context';
 import {Link} from 'react-router-dom';
 import {ButtonContainer} from './Button';
 
 export default class Details extends Component {
   render() {
     return (
-      <div>
-        <h3>Hello from details</h3>
-      </div>
+      <ProductConsumer>
+        {(value)=>{
+          const {id,company,img,info,price,title,inCart} = value.detailProduct;
+          return (
+            <div className="container py-5">
+            {/* title*/}
+            <div className="row">
+              <div className="col-10 mx-auto text-center text-slanted text-blue my-5">
+                <h1>{title}</h1>
+              </div>
+            </div>
+            {/* end title*/}
+            </div>
+          )
+        }}
+      </ProductConsumer>
     )
   }
 }
