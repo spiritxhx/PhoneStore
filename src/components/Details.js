ede912df4ac98a99fb10c4a764199e47d66ed879
spiritxhx <spiritxhx@gmail.com>
2019-05-09 22:33:32 -0400

    add the image to the details page

--- src/components/Details.js
+++ src/components/Details.js
@@ -18,6 +18,15 @@ export default class Details extends Component {
               </div>
             </div>
             {/* end title*/}
+            {/* Product info */}
+            <div className="row">
+              <div className="col-10 mx-auto col-md-6 my-3">
+                <img src={img} className="img-fluid" alt="product" />
+              </div>
+              <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
+              
+              </div>
+            </div>
             </div>
           )
         }}
