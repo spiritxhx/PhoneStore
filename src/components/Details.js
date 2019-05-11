6467cbeffec3f0b4edb1086e1caaecf03701c177
spiritxhx <spiritxhx@gmail.com>
2019-05-10 22:40:46 -0400

    add the product text

--- src/components/Details.js
+++ src/components/Details.js
@@ -23,8 +23,9 @@ export default class Details extends Component {
               <div className="col-10 mx-auto col-md-6 my-3">
                 <img src={img} className="img-fluid" alt="product" />
               </div>
+              {/* prodct text */}
               <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
-              
+                <h2>model: {title}</h2>
               </div>
             </div>
             </div>
