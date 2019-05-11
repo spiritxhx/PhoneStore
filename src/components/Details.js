ebb6cbc56b3a57ed2a9c329de84423877dc2c19a
spiritxhx <spiritxhx@gmail.com>
2019-05-10 22:59:03 -0400

    add the product info

--- src/components/Details.js
+++ src/components/Details.js
@@ -26,6 +26,20 @@ export default class Details extends Component {
               {/* prodct text */}
               <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
                 <h2>model: {title}</h2>
+                <h4 className="text-title text-uppercase text-muted mt-3 mb-2">
+                  made by: <span className="text-uppercase">{company}</span>
+                </h4>
+                <h4 className="text-blue">
+                  <strong>
+                    price : <span>${price}</span>
+                  </strong>
+                </h4>
+                <p className="text-capitalize font-weight-bold mt-3 mb-0">
+                  some info about product: 
+                </p>
+                <p className="text-muted lead">
+                  {info}
+                </p>
               </div>
             </div>
             </div>
