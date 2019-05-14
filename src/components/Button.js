3a008b6f3f34dba5cc27cc6782bb98d3f63afc3f
spiritxhx <spiritxhx@gmail.com>
2019-05-13 23:42:26 -0400

    change the color of the button

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,8 +5,8 @@ export const ButtonContainer = styled.button`
     font-size:1.4rem;
     background: transparent;
     border: 0.05rem solid var(--lightBlue);
-    border-color: ${props => (props.cart ? "var(--mainYellow)": "var(--lightBlue)")};
-    color: var(--lightBlue);
+    border-color: ${props => props.cart ? "var(--mainYellow)": "var(--lightBlue)"};
+    color: ${props => props.cart?"var(--mainYellow)": "var(--lightBlue)"};
     border-radius: 0.5rem;
     padding: 0.2rem 0.5rem; 
     cursor: pointer;
