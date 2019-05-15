32a82f15966edf66c8ce847f3f0b9a45a1078e1a
spiritxhx <spiritxhx@gmail.com>
2019-05-14 22:58:11 -0400

    render the handleDetail method and it works well

--- src/components/Product.js
+++ src/components/Product.js
@@ -10,7 +10,13 @@ export default class Product extends Component {
     return (
       <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
         <div className="card">
-          <div className="img-container p-5" onClick={() => console.log('You clicked on the image container')}>
+        <ProductConsumer>
+          {(value) =>(
+          <div 
+            className="img-container p-5" 
+            onClick={() => 
+              value.handleDetail(id)
+            }>
             <Link to="/details">
             <img src={img} alt="product" className="card-img-top"></img>
             </Link>
@@ -18,8 +24,8 @@ export default class Product extends Component {
               className="cart-btn" 
               disabled={inCart?true: false} 
               onClick={()=>{
-                console.log('added to the cart'
-              )} } 
+                value.addToCart(id)
+              } } 
             >
               {inCart?
                 (<p className="text-capitalize mb-0" disabled>
@@ -29,7 +35,9 @@ export default class Product extends Component {
                   <i className="fas fa-cart-plus" />
                 )}
             </button>
-          </div>
+          </div>)}
+          
+        </ProductConsumer>
           {/* card footer*/}
           <div className="card-footer d-flex justify-content-between">
               <p className="aligh-self-center mb-0">
--- src/context.js
+++ src/context.js
@@ -29,8 +29,11 @@ class ProductProvider extends Component {
         return product;
     }
 
-    handleDetail =() => {
-        console.log('hello from detail');
+    handleDetail =(id) => {
+        const product = this.getItem(id);
+        this.setState(()=>{
+            return {detailProduct:product}
+        })
     };
     addToCart = (id) =>{
         console.log(`hello from add to cart.id is ${id}`);
