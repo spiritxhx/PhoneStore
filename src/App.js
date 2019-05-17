2e1e18c149f66f2b47dc6dbb55a80984608102b1
spiritxhx <spiritxhx@gmail.com>
2019-05-16 22:40:02 -0400

    import all the stuffs in Modal.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {Switch,Route} from 'react-router-dom';
-import logo, { ReactComponent } from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,4 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import {ProductConsumer} from '../context';
+import {ButtonContainer} from './Button';
+import {Link} from 'react-router-dom';
+
 
 export default class Modal extends Component {
   render() {
