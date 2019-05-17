a12b642a4530bc4a94b5ced4550af45798c6cf12
spiritxhx <spiritxhx@gmail.com>
2019-05-16 21:09:29 -0400

    init modify the addToCart method

--- src/context.js
+++ src/context.js
@@ -36,7 +36,15 @@ class ProductProvider extends Component {
         })
     };
     addToCart = (id) =>{
-        console.log(`hello from add to cart.id is ${id}`);
+        let tempProducts = [...this.state.products];
+        const index = tempProducts.indexOf(this.getItem(id));
+        const product = tempProducts[index];
+        product.inCart = true;
+        product.count = 1;
+        const price = product.price;
+        product.total = price;
+
+        
     };
   render() {
     return (
