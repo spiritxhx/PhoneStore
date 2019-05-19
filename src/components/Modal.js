2362f4841278e28442287f732646fbce18173719
spiritxhx <spiritxhx@gmail.com>
2019-05-18 23:54:36 -0400

    add buttons in modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -22,9 +22,19 @@ export default class Modal extends Component {
                       <div className="row">
                           <div id="modal" className="col-8 mx-auto col-md-6 col-lg-4 text-center text-capitalize">
                               <h5>item added to the cart</h5>
-                              <img src={img} className="img-fluid" alt=""product />
+                              <img src={img} className="img-fluid" alt="product" />
                               <h5>{title}</h5>
                               <h5 className="text-muted">price : $ {price}</h5>
+                              <Link to="/">
+                                <ButtonContainer onClick={()=>closeModal()}>
+                                    store
+                                </ButtonContainer>
+                              </Link>
+                              <Link to="/cart">
+                                <ButtonContainer cart onClick={()=>closeModal()}>
+                                    go to cart
+                                </ButtonContainer>
+                              </Link>
                           </div>
                       </div>
                     </div>
