49b5699050e90c085e17728aa8e2571fb578e3cc
spiritxhx <spiritxhx@gmail.com>
2019-05-20 23:09:58 -0400

    create EmptyCart.js and set conditional rendering in Cart.js

--- src/components/Cart/CartColumns.js
+++ src/components/Cart/CartColumns.js
@@ -8,21 +8,21 @@ export default function CartColumns() {
               <p className="text-uppercase">products</p>
           </div>
           <div className="col-10 mx-auto col-lg-2">
-              <p className="text-uppercase">products</p>
+              <p className="text-uppercase">name of product</p>
           </div>
           <div className="col-10 mx-auto col-lg-2">
-              <p className="text-uppercase">products</p>
+              <p className="text-uppercase">price</p>
           </div>
           <div className="col-10 mx-auto col-lg-2">
-              <p className="text-uppercase">products</p>
+              <p className="text-uppercase">quantity</p>
           </div>
           <div className="col-10 mx-auto col-lg-2">
-              <p className="text-uppercase">products</p>
+              <p className="text-uppercase">remove</p>
           </div>
           <div className="col-10 mx-auto col-lg-2">
-              <p className="text-uppercase">products</p>
+              <p className="text-uppercase">total</p>
           </div>
-      </div>
+      </div>  
     </div>
   )
 }
