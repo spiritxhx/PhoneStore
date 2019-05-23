7ce75738b44f55d81c7d9cc1d2ec0bd3a9891f7f
spiritxhx <spiritxhx@gmail.com>
2019-05-22 23:30:15 -0400

    add the 1st column in cartlist

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
-export default function CartItem() {
+export default function CartItem({item,value}) {
+    const {id,title,img,price,total,count}=item;
+    const {increment,decrement,removeItem} =value;
   return (
-    <div>
-      this is a cart item
+    <div className="row my-1 text-capitalize text-center">
+      <div className="col-10 mx-auto col-lg-2">
+        <img src={img} style={{width:'5rem',height:"5rem"}} className="img-fluid" alt="product" />
+      </div>
     </div>
   )
 }
