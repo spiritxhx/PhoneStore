cb69d5d92ef8671a1165271c0381b39c45b13a7b
spiritxhx <spiritxhx@gmail.com>
2019-05-23 23:44:34 -0400

    add the remove button in cartlist

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -29,6 +29,12 @@ export default function CartItem({item,value}) {
       </div>
 
       <div className="col-10 mx-auto col-lg-2">
+        <div className="cart-icon" onClick={()=>removeItem(id)}>
+          <i className="fas fa-trash"></i>
+        </div>
+      </div> 
+
+      <div className="col-10 mx-auto col-lg-2">
         <strong> item total : $ {total}</strong>
       </div>   
     </div>
