2054316b063734efa12595e26da0f3e9b5c5191e
spiritxhx <spiritxhx@gmail.com>
2019-05-24 22:22:56 -0400

    change the cart state back to empty to check if our cartlist works

--- src/context.js
+++ src/context.js
@@ -9,7 +9,7 @@ class ProductProvider extends Component {
     state = {
         products: [], 
         detailProduct: detailProduct, 
-        cart: storeProducts, 
+        cart: [], 
         modalOpen: false, 
         modalProduct: detailProduct, 
         cartSubTotal: 0,
