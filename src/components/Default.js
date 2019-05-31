9f4cbed79ed924ad3b885f931f567c08c8e87433
spiritxhx <spiritxhx@gmail.com>
2019-05-30 23:04:53 -0400

    start working on the default page

--- src/components/Default.js
+++ src/components/Default.js
@@ -2,9 +2,15 @@ import React, { Component } from 'react'
 
 export default class Default extends Component {
   render() {
+    console.log(this.props);
+    
     return (
-      <div>
-        <h3>Page not found</h3>
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto text-center text-title text-uppercase pt-5">
+            <h1 className="display-3">404</h1>
+          </div>
+        </div>
       </div>
     )
   }
