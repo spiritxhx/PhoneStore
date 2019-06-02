d2451a938fbd40a1011d521c22bf074cb489c510
spiritxhx <spiritxhx@gmail.com>
2019-06-01 21:52:25 -0400

    personalize for the deployment

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -10,7 +10,7 @@ export default class ProductList extends Component {
             <React.Fragment>
                 <div className="py-5">
                     <div className="container">
-                    <Title name="our" title="products" />
+                    <Title name="Hanxiang's" title="products" />
                         <div className="row">
                             <ProductConsumer>
                                 {value => {
