fe1c8e3089929436336b6014510881020aac4eec
spiritxhx <spiritxhx@gmail.com>
2019-06-06 23:59:40 -0400

    edit the total payment to the props.total

--- src/components/Cart/PaypalButton.js
+++ src/components/Cart/PaypalButton.js
@@ -41,7 +41,7 @@ export default class MyApp extends React.Component {
 
     // NB. You can also have many Paypal express checkout buttons on page, just pass in the correct amount and they will work!
     return (
-      <PaypalExpressBtn env={env} client={client} currency={currency} total={total} onError={onError} onSuccess={onSuccess} onCancel={onCancel} />
+      <PaypalExpressBtn env={env} client={client} currency={currency} total={this.props.total} onError={onError} onSuccess={onSuccess} onCancel={onCancel} />
     );
   }
 }
